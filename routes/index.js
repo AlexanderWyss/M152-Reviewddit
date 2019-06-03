fea704089cd88862c92523ce164e57fa872e4e50
Alexander Wyss <alexander.wyss@bison-group.com>
2019-06-03 11:53:11 +0200

    node and scss support

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path =  require('path');
+var router = express.Router();
+
+function html(file) {
+  return path.join(__dirname, '../views/', file);
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(html('./index.html'));
+});
+
+router.get('/create-review', function(req, res, next) {
+  res.sendFile(html('./createReview.html'));
+});
+
+
+module.exports = router;
