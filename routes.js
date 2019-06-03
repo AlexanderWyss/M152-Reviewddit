bc4f194de92537499447aace5b489fe2ba506f8e
Alexander Wyss <alexander.wyss@bison-group.com>
2019-06-03 16:05:04 +0200

    own simple layout implementation

--- routes.js
+++ routes.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var path =  require('path');
 var router = express.Router();
+var fs = require('fs');
 
-function html(file) {
-  return path.join(__dirname, 'views/', file);
+function returnView(file, res) {
+  var file = fs.readFile(path.join(__dirname, 'views/', file), 'utf8', (err, html) => {
+    if (err) throw err;
+    fs.readFile(path.join(__dirname, 'views/header.html'), 'utf8', (err, headerHtml) => {
+      if (err) throw err;
+      res.send(html.replace('<headerPlaceholder/>', headerHtml));
+    });
+  });
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(html('./index.html'));
+  returnView('./index.html', res);
 });
 
 router.get('/create-review', function(req, res, next) {
-  res.sendFile(html('./createReview.html'));
+  returnView('./createReview.html', res);
 });
-
+ 
 
 module.exports = router;
