c5e71790a41bf0e0617464e3495db428686f905c
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-06-20 20:42:06 +0200

    fix autoresize bug

--- public/javascripts/createReview.js
+++ public/javascripts/createReview.js
@@ -2,7 +2,7 @@ Array.from(document.getElementsByClassName('autoresize')).forEach(function (elem
     element.style.boxSizing = 'border-box';
     element.style.resize = 'none';
     var offset = element.offsetHeight - element.clientHeight;
-    document.addEventListener('input', function (event) {
+    element.addEventListener('input', function (event) {
         event.target.style.height = 'auto';
         event.target.style.height = event.target.scrollHeight + offset + 'px';
     });
