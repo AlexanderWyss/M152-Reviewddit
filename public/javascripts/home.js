0c224fc22e2c138d32b65befefd69726e15db93d
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-06-20 17:53:02 +0200

    working reviewdetails

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,50 +1,47 @@
-
 var postElement = document.getElementsByClassName('post')[0].cloneNode(true)
 postElement.removeAttribute("hidden");
 var post = postElement.outerHTML;
-var contents = [
-    { title: 'Image 1', content: `<img src="images/placeholder.png">` },
-    {
-        title: 'What is Lorem Ipsum', content:
-            `<p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-            of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-            but also the leap into electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus PageMaker including versions of
-            Lorem Ipsum.
-            </p>`
-    },
-    { title: 'Audio', content: `<audio src="audio/audio.wav" controls></audio>` },
-    { title: 'Video', content: `<video src="video/video.mp4" controls></video>` },
-    { title: 'Image 2', content: `<img src="images/index.png">` }
-];
 var home = document.getElementById('home-container');
-
-for (var i = 0; i < 20; i++) {
-    if (i < contents.length) {
-        addPost(i);
-    } else {
-        addPost();
-    }
-}
+var contents;
 
 function addPost(contentIndex = Math.floor(Math.random() * contents.length)) {
     var content = contents[contentIndex];
     home.insertAdjacentHTML('beforeend',
         post
-            .replace(/{{title}}/g, content.title)
-            .replace(/{{content}}/g, content.content)
-            .replace(/{{score}}/g, Math.floor(Math.random() * 1000))
-            .replace(/{{number-of-comments}}/, Math.floor(Math.random() * 100))
+        .replace(/{{title}}/g, content.title)
+        .replace(/{{file}}/g, content.file)
+        .replace(/{{text}}/g, content.text)
+        .replace(/{{score}}/g, Math.floor(Math.random() * 1000))
+        .replace(/{{number-of-comments}}/g, Math.floor(Math.random() * 100))
+        .replace(/{{id}}/g, contentIndex)
     );
 }
 
-window.onscroll = function (ev) {
-    if ((window.innerHeight * 2 + window.scrollY) >= home.offsetHeight) {
-        for (var i = 0; i < 5; i++) {
+function addContent() {
+    for (var i = 0; i < 20; i++) {
+        if (i < contents.length) {
+            addPost(i);
+        } else {
             addPost();
         }
     }
+
+    window.onscroll = function (ev) {
+        if ((window.innerHeight * 2 + window.scrollY) >= home.offsetHeight) {
+            for (var i = 0; i < 5; i++) {
+                addPost();
+            }
+        }
+    };
+}
+
+var httpRequest = new XMLHttpRequest();
+
+httpRequest.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        contents = JSON.parse(this.responseText);
+        addContent();
+    }
 };
+httpRequest.open('GET', '/allPosts', true);
+httpRequest.send();
--- routes.js
+++ routes.js
@@ -2,17 +2,18 @@ var express = require('express');
 var path =  require('path');
 var router = express.Router();
 var fs = require('fs');
+var content = require('./content');
 
 function readFile(file, callback) {
   fs.readFile(path.join(__dirname, 'views/', file), 'utf8', callback);
 }
 
-function returnView(name, res, title='Reviewddit') {
+function returnView(name, res, title='Reviewddit', apply = (html) => html) {
   readFile('_layout.html', (err, layoutHtml) => {
     layoutHtml = layoutHtml.replace(/{{stylesheet-placeholder}}/g, name)
                            .replace(/{{title-placeholder}}/g, title);
     readFile(`${name}.html`, (err, html) => {
-      res.send(layoutHtml.replace(/{{body-placeholder}}/g, html));
+      res.send(apply(layoutHtml.replace(/{{body-placeholder}}/g, html)));
     });
   });
 }
@@ -21,11 +22,21 @@ function returnView(name, res, title='Reviewddit') {
 router.get('/', function(req, res, next) {
   returnView('home', res);
 });
-router.get('/review-details', function(req, res, next) {
-  returnView('reviewDetails', res, 'Review Details');
+router.get('/review-details/:id', function(req, res, next) {
+  returnView('reviewDetails', res, 'Review Details', function (html) {
+    var post = content.posts[req.params.id];
+    return html.replace(/{{title}}/g, post.title)
+        .replace(/{{file}}/g, post.file)
+        .replace(/{{text}}/g, post.text)
+        .replace(/{{score}}/g, Math.floor(Math.random() * 1000))
+  });
 });
 router.get('/create-review', function(req, res, next) {
   returnView('createReview', res, 'Create Review');
+});
+
+router.get('/allPosts', function(req, res, next) {
+  res.send(content.posts);
 });
  
 
